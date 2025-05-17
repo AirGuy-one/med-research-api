e33777997f5026b5d8dec1e209b736b7d7477928
Kirill <billy@MacBook-Air.local>
2025-05-17 22:57:49 +0300

    Add dot env

--- src/database.py
+++ src/database.py
@@ -1,10 +1,14 @@
+import os
 from typing import Generator
 
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:postgres@127.0.0.1:5432/postgres"
+load_dotenv()
+
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_engine(
     DATABASE_URL,
